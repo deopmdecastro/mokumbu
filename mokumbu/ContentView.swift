22232f8ad96d1e52042a51c931e085843d99e8f6
deopmdecastro <deopmdecastro@gmail.com>
2023-09-20 10:49:02 +0100

    Initial Commit

---
+++ mokumbu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mokumbu
+//
+//  Created by Deogracia de Castro (WEY MEDIA) on 20/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mokumbu/mokumbuApp.swift
@@ -0,0 +1,17 @@
+//
+//  mokumbuApp.swift
+//  mokumbu
+//
+//  Created by Deogracia de Castro (WEY MEDIA) on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct mokumbuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
