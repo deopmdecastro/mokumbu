8aef35aee7c7bc42e6cdd8ad13b506894a086b71
deopmdecastro <deopmdecastro@gmail.com>
2023-09-22 13:37:38 +0100

    mudanças

---
+++ mokumbu/pages/testeLogin.swift
@@ -0,0 +1,18 @@
+//
+//  testeLogin.swift
+//  mokumbu
+//
+//  Created by Deogracia de Castro (WEY MEDIA) on 22/09/2023.
+//
+
+import SwiftUI
+
+struct testeLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testeLogin()
+}
